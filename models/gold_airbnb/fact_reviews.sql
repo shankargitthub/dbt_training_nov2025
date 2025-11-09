7ad384daa184d7ae2d97d69766f334e71ac07747
Shankar Bhagat <bhagat1983@hotmail.com>
2025-11-09 15:14:33 +0530

    fact_review changes

--- models/gold_airbnb/fact_reviews.sql
+++ models/gold_airbnb/fact_reviews.sql
@@ -1,2 +1,16 @@
-select * from {{ref('silver_reviews')}}
---dev.silver_airbnb.silver_hosts
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = ['listing_id','reviewer_name'],
+    cluster_by = ['REVIEW_DATE'],  
+    incremental_strategy = 'merge',
+    incremental_predicates = [
+      "DBT_INTERNAL_DEST.REVIEW_DATE > dateadd(day, -30, '2021-10-28')"
+    ]
+  )
+}}
+
+select * from  {{ref('silver_reviews')}}
+{% if is_incremental() %}
+where review_date > (select max(review_date) from {{this}})
+{% endif %}
